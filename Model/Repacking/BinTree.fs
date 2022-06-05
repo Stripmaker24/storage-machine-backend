d70796d12a0267fa0f9adc7d241f677777fb8935
Stripmaker24 <33493759+Stripmaker24@users.noreply.github.com>
2022-06-05 11:25:47 +0200

    Update BinTree.fs

--- Model/Repacking/BinTree.fs
+++ Model/Repacking/BinTree.fs
@@ -3,11 +3,17 @@
 open StorageMachine
 open Common
 
+type PackageType = 
+    | BubbleWrap
+    | CardBoard
+
 type BinTree =
     | Bin of BinIdentifier * List<BinTree>
     | Product of PartNumber
+    | PackagedProduct of PartNumber * PackageType
 
 let rec productCount binTree =
     match binTree with
     | Bin (_, productsOrBins) -> List.sumBy productCount productsOrBins
-    | Product _ -> 1
+    | Product _ | PackagedProduct _ -> 1
+    
